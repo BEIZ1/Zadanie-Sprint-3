7d4b25939a3bb71308c45a8b79a82a6db6db8646
igizxabibullin <ilgizxabibullin@yandex.ru>
2023-10-24 23:39:58 +0500

    Добавил логи исправил констрейенты, доработал усложненный проект

--- Zadanie Sprint 3/ViewController.swift
+++ Zadanie Sprint 3/ViewController.swift
@@ -6,21 +6,38 @@
 //
 
 import UIKit
-
+import Foundation
 class ViewController: UIViewController {
 
+    // - MARK: - Переменные
     private var clicks: Int = 0
-    
+    private var logs: [String] = []
+
+    // - MARK: - Outlet
+   
     @IBOutlet weak var Counter: UILabel!
+    @IBOutlet weak var losOnConter: UITextView!
+    
+    // - MARK: - Func
+    
+        
+    func logsOnTextView(){
+        let data = Date()
+        losOnConter.text += "Значение было изменино на \(clicks) Время: \(data)" + "\n"
+    }
+
+    // - MARK: - Action
     
     @IBAction func buttonEraser(_ sender: Any) {
         clicks = 0
         Counter.text = "Значение счетчика: \(clicks)"
+        logsOnTextView()
     }
     @IBAction func buttonMinus(_ sender: Any) {
         if clicks > 0{
             clicks -= 1
             Counter.text = "Значение счетчика: \(clicks)"
+            logsOnTextView()
         }else{
             Counter.text = "Ошибка: Значение счетчика не может быть меньше \(clicks)"
         }
@@ -28,15 +45,19 @@ class ViewController: UIViewController {
     @IBAction func buttonPlus(_ sender: Any) {
         clicks += 1
         Counter.text = "Значение счетчика: \(clicks)"
+        logsOnTextView()
     }
     @IBAction func buttonOnClick(_ sender: Any) {
         clicks += 1
         Counter.text = "Значение счетчика: \(clicks)"
+        logsOnTextView()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        losOnConter.isEditable = false
+        losOnConter.scrollsToTop = true
     }
 
 

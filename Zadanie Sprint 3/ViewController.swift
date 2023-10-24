e2646f19e81ec8e4fe820e1dbf4ca1d728d3f93f
igizxabibullin <ilgizxabibullin@yandex.ru>
2023-10-24 15:12:31 +0500

    Обычное задание спринта 3

--- Zadanie Sprint 3/ViewController.swift
+++ Zadanie Sprint 3/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var clicks: Int = 0
+    
+    @IBOutlet weak var Counter: UILabel!
+    
+    @IBAction func buttonOnClick(_ sender: Any) {
+        clicks += 1
+        Counter.text = "Значение счетчика: \(clicks)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

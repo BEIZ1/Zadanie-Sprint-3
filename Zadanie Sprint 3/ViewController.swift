80f11802a6714261329fc239e815c0c2693b97a0
igizxabibullin <ilgizxabibullin@yandex.ru>
2023-10-24 14:54:59 +0500

    Initial Commit

---
+++ Zadanie Sprint 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Zadanie Sprint 3
+//
+//  Created by Ильгиз Хабибуллин on 24/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
